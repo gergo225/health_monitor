95ceb965e3b83781bb72783aefca2a2068253c9b
Gergő Fazekas <fazekasgergo225@gmail.com>
2022-07-11 11:01:47 +0300

    Initial commit. Add two Fragments and Navigation

---
+++ app/src/main/java/com/fazekasgergo/healthmonitor/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.fazekasgergo.healthmonitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fazekasgergo.healthmonitor.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/fazekasgergo/healthmonitor/pages/main/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.fazekasgergo.healthmonitor.pages.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.fazekasgergo.healthmonitor.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        binding.nextPageButton.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(MainFragmentDirections.actionMainFragmentToQuestionFragment())
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
