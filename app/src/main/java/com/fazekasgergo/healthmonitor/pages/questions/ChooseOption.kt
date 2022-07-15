fc9b8db7710bb5a17471b50d285f181188506c8e
Gergő Fazekas <fazekasgergo225@gmail.com>
2022-07-15 10:58:09 +0300

    Add more classes to choose options (diet, work environment, blood pressure, family disease history)

--- app/src/main/java/com/fazekasgergo/healthmonitor/pages/questions/ChooseOption.kt
+++ app/src/main/java/com/fazekasgergo/healthmonitor/pages/questions/ChooseOption.kt
@@ -47,3 +47,49 @@ enum class AlcoholConsumption(private val consumption: String) : ChooseOption {
         return this.consumption
     }
 }
+
+enum class Food(private val food: String) : ChooseOption {
+    FRUITS_VEGETABLES("Fruits & vegetables"),
+    MEAT("Meat"),
+    VEGETARIAN("Vegetarian"),
+    FAST_FOOD("Fast Food");
+
+    override fun text(): String {
+        return this.food
+    }
+}
+
+enum class Work(private val environment: String) : ChooseOption {
+    TOXIC("Toxi"),
+    STRESSFUL("Stressful"),
+    SEDENTARY("Sedentary"),
+    COMMON_LABOR("Common labor");
+
+    override fun text(): String {
+        return environment
+    }
+}
+
+enum class BloodPressure(private val pressure: String) : ChooseOption {
+    LOW("Low"),
+    NORMAL("Normal"),
+    HIGH("High"),
+    DONT_KNOW("I don't know");
+
+    override fun text(): String {
+        return pressure
+    }
+}
+
+enum class FamilyDiseases(private val disease: String) : ChooseOption {
+    DIABETES("Diabetes"),
+    CARDIOVASCULAR("Cardiovascular diseases"),
+    LIVER("Liver diseases"),
+    KIDNEY("Kidney diseases");
+
+    override fun text(): String {
+        return disease
+    }
+}
+
+
